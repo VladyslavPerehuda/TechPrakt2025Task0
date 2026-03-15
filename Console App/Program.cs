0c67a41503ef2fd772c1ae9964ebf16753b12f63
dalv2009 <ipz232_pva@student.ztu.edu.ua>
2026-03-15 16:55:35 +0100

    add console app

---
+++ Console App/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter a:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter b:");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Sum = " + (a + b));
